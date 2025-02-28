7404ab032b1fa2a1f40181619230e87aefee1fd6
mgrispino <mgrispino@fordham.edu>
2025-02-28 12:48:01 -0500

    pushing 2D sprite changes

--- 2d-game/Scenes/character_body_2d.gd
+++ 2d-game/Scenes/character_body_2d.gd
@@ -1,25 +1,33 @@
 extends CharacterBody2D
 
+@export var speed = 5
+var jump = false
+@onready var sprite = $AnimatedSprite2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("Move_Right"):
+		position.x =+ speed
+		sprite.play("Walk")
+		sprite.flip_h = false
+	elif Input.is_action_pressed("Move_Left"):
+		position.x -= speed
+		sprite.play("Walk")
+		sprite.flip_h = true
+	elif Input.is_action_pressed("Jump") or jump:
+		sprite.play("Jump")
+		jump = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		sprite.play("Idle")
+	
+	pass
 
-	move_and_slide()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "Jump":
+		jump = false
+	pass # Replace with function body.
