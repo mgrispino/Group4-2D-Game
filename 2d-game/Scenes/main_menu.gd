5b1fd6aa3a3fe0c6dcaa6ab3298853d7768b8cb9
mgrispino <mgrispino@fordham.edu>
2025-03-05 12:29:34 -0500

    Fixed up more of the main menu & got rid of the test blocks in the character scene

--- 2d-game/Scenes/main_menu.gd
+++ 2d-game/Scenes/main_menu.gd
@@ -3,6 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$VBoxContainer/Label/VBoxContainer/Start.grab_focus()
+	
 	pass # Replace with function body.
 
 
@@ -11,5 +13,10 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
+	get_tree().change_scene_to_file("res://level 1.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
 	pass # Replace with function body.
