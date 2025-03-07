7a4c372e092c4cc0eed6ca2c9ce631c374ad915f
mgrispino <mgrispino@fordham.edu>
2025-03-07 11:14:25 -0500

    Control Scene Pretty much done & Fixed 2D character hitboxing

--- 2d-game/Scenes/main_menu.gd
+++ 2d-game/Scenes/main_menu.gd
@@ -3,20 +3,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxContainer/Label/VBoxContainer/Start.grab_focus()
 	
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://level 1.tscn")
 	pass # Replace with function body.
 
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Controls.tscn")
+	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	pass # Replace with function body.
+	
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
 	pass # Replace with function body.
