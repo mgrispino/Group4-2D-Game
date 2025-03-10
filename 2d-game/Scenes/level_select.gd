b2564628fa35f10c0e53135c602420b2181a671b
mgrispino <mgrispino@fordham.edu>
2025-03-10 03:31:46 -0400

    Music fully done & added Level selection screen

---
+++ 2d-game/Scenes/level_select.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level 1.tscn")
+	pass # Replace with function body.
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+	pass # Replace with function body.
+
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level 3.tscn")
+	pass # Replace with function body.
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	pass # Replace with function body.
